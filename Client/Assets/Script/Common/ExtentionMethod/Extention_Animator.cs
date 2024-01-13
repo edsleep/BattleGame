7508d7b8fd69957d89b511185944b62793f16e20
edsleep <red990217@naver.com>
2024-01-13 20:47:29 +0900

    유니티 파일 정리

---
+++ Client/Assets/Script/Common/ExtentionMethod/Extention_Animator.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Extention_Animator
+{
+    public static bool IsAnimationFinished(this Animator self)
+    {
+        if(null == self)
+            return false;
+
+        return 1 <= self.GetCurrentAnimatorStateInfo(0).normalizedTime;
+    }
+
+    public static float GetLength(this Animator self)
+    {
+        if (null == self)
+            return 0;
+
+        return self.GetCurrentAnimatorStateInfo(0).length;
+    }
+
+    public static float GetCurrentPlayTime(this Animator self)
+    {
+        if (null == self)
+            return 0;
+
+        return self.GetCurrentAnimatorStateInfo(0).normalizedTime * self.GetCurrentAnimatorStateInfo(0).length;
+    }
+
+    public static float GetCurrentPlayTimeNormalized(this Animator self)
+    {
+        if (null == self)
+            return 0;
+
+        return self.GetCurrentAnimatorStateInfo(0).normalizedTime;
+    }
+}
